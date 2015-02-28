11d560839ba445c3d45cd9500da5b18e2f43ac05
Justin M <justinjams@gmail.com>
2015-02-28 01:19:30 -0500

    Mute: Restore Mute functionality through RIL for legacy devices [2/2]
    This restores the old Mute function for devices that do not support
    muting the microphone through the system audio manager.
    
    Disable the Media Button callback, and send a HEADSETHOOK
    Keyevent when the in-call mute button is pressed.
    
    Issues with this method are that the in-call microphone button never gets
    highlighted when we are in mute - but the statusbar icon will indicate whether
    we are in a mute state.
    
    add new build.prop entry to enable this: ro.telephony.legacy_mute=1
    
    (See Telephony for part 1/2)
    
    Good enough for now....
    
    Change-Id: I873fb8cbc9d2e37e601c005450d0078d5fc8cab1

--- src/com/android/server/telecom/HeadsetMediaButton.java
+++ src/com/android/server/telecom/HeadsetMediaButton.java
@@ -20,6 +20,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.AudioAttributes;
 import android.media.session.MediaSession;
+import android.os.SystemProperties;
 import android.view.KeyEvent;
 
 /**
@@ -49,14 +50,14 @@ final class HeadsetMediaButton extends CallsManagerListenerBase {
             return true;
         }
     };
-
+    
     private final CallsManager mCallsManager;
 
     private final MediaSession mSession;
 
     HeadsetMediaButton(Context context, CallsManager callsManager) {
         mCallsManager = callsManager;
-
+	
         // Create a MediaSession but don't enable it yet. This is a
         // replacement for MediaButtonReceiver
         mSession = new MediaSession(context, HeadsetMediaButton.class.getSimpleName());
@@ -87,7 +88,11 @@ final class HeadsetMediaButton extends CallsManagerListenerBase {
     @Override
     public void onCallAdded(Call call) {
         if (!mSession.isActive()) {
-            mSession.setActive(true);
+	    if (SystemProperties.getInt("ro.telephony.legacy_mute", 0) == 1) {
+		return;
+	    } else {
+		mSession.setActive(true);
+	    }
         }
     }
 
